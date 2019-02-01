e7922aaf2c855574cd1268ee23de39d0d9d0de27
Nick Farley <nick-1154@hotmail.com>
2019-01-31 18:12:10 -0600

    Created all classes

---
+++ src/main/java/frc/actions/JoinActions.java
@@ -0,0 +1,57 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.actions;
+
+/**
+ * Add your docs here.
+ */
+public class JoinActions implements Actionable{
+
+    private final Actionable[] actions;
+
+    public JoinActions() {
+        actions = new Actionable[]{new NullAction()};
+    }
+
+    public JoinActions(Actionable... actions) {
+        this.actions = actions;
+    }
+
+    @Override
+    public void startAction() {
+        for (Actionable action: actions) {
+            action.startAction();
+        }
+    }
+
+    @Override
+    public void periodic() {
+        for (Actionable action: actions) {
+            action.startAction();
+        }
+    }
+
+    @Override
+    public void endAction() {
+        for (Actionable action: actions) {
+            action.startAction();
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        boolean finished = true;
+        for (Actionable action: actions) {
+            if (!action.isFinished()) {
+                finished = false;
+            }
+        }
+        return finished;
+    }
+}
+
