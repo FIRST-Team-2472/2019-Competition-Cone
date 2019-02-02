261c4a9214b80f5881befdc9e21bc1ac3f29df1b
undisilver <undisilver@gmail.com>
2019-02-02 14:04:16 -0600

    Create RaiseFront.java

---
+++ src/main/java/frc/actions/RaiseFront.java
@@ -0,0 +1,39 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.actions;
+
+/**
+ * Add your docs here.
+ */
+public class RaiseFront implements Actionable {
+    public RaiseFront()
+    {
+        
+    }
+
+    @Override
+    public void startAction() {
+
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+
+    @Override
+    public void endAction() {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
+
